d5bcf1c65586bbfbec18076ae856c3216fcbfc27
Alexander <baks750@mail.ru>
2022-06-14 14:05:12 +0300

    Исправил ошибки и ввёл индекс текущего слайда

--- src/modules/infinity-slider.js
+++ src/modules/infinity-slider.js
@@ -7,8 +7,16 @@ export default class InfinitySlider {
     this.leftButton = this.root.querySelector(`.slider__button--prev`);
     this.rightButton = this.root.querySelector(`.slider__button--next`);
 
+    this._slideWidth = this.slides[0].offsetWidth;
+    this._wrapperWidth = this.wrapper.offsetWidth;
+    this._wrapperPadding = parseInt(window.getComputedStyle(this.wrapper, null).getPropertyValue(`padding-left`));
+    
+    this._SLIDER_WIDTH = this._wrapperWidth - this._wrapperPadding;
+    this._SLIDES_NUMBER = this._SLIDER_WIDTH / this._slideWidth;
+
     this._shift = 0;
     this._left = 0;
+    this._slideIndex = 0;
   }
 
   init() {
@@ -16,43 +24,54 @@ export default class InfinitySlider {
     this._setLeftButtonClickHandler();
   }
 
-  _setLeftButtonClickHandler() {
+  _setLeftButtonClickHandler() { // ok
     this.leftButton.addEventListener(`click`, () => {
-      this._addSlidesToTheBegining();
+      (this._slideIndex > this._SLIDES_NUMBER) ? this._slideIndex -= this._SLIDES_NUMBER : ``;
+      
+      if (this._slideIndex <= this._SLIDES_NUMBER) {
+        this._addSlidesToTheBegining();
+        this._left -= this._SLIDER_WIDTH;
+        
+        this.list.style.left = this._left + `px`; 
+      }
 
-      this._left += -1180;
-      this._shift += 1180;
-
-      this.list.style.left = this._left + `px`;  
-      this.list.style.transform = `translateX(${this._shift}px)`; 
-    });
-  }
-
-  _setRightButtonClickHandler() {
-    this.rightButton.addEventListener(`click`, () => {
-      this._addSlidesToTheEnd();
-
-      this._left += 1180;
-      this._shift += -1180;
-
-      this.list.style.left = this._left + `px`;  
+      this._shift += this._SLIDER_WIDTH; 
       this.list.style.transform = `translateX(${this._shift}px)`;
+
+      console.log(this._slideIndex);
     });
   }
 
-  _addSlidesToTheEnd() {
-    const firstSlides = this.slides.splice(0, 5);
+  _setRightButtonClickHandler() { // ok
+    this.rightButton.addEventListener(`click`, () => {
+      (this._slideIndex < this.slides.length) ? this._slideIndex += this._SLIDES_NUMBER : ``;
+      
+      if (this._slideIndex >= this.slides.length) {
+        this._addSlidesToTheEnd();
+        this._left += this._SLIDER_WIDTH;
+
+        this.list.style.left = this._left + `px`;
+      } 
+        
+      this._shift -= this._SLIDER_WIDTH;
+      this.list.style.transform = `translateX(${this._shift}px)`;
+
+      console.log(this._slideIndex);
+    });
+  }
+
+  _addSlidesToTheEnd() { // ok
+    const firstSlides = this.slides.splice(0, this._SLIDES_NUMBER);
     this.slides = [].concat(this.slides, firstSlides);
     
     firstSlides.forEach((slide) => this.list.insertAdjacentElement(`beforeend`, slide));
   }
 
-  _addSlidesToTheBegining() {
-    const lastSlides = this.slides.splice(this.slides.length - 5, 5);
+  _addSlidesToTheBegining() { // ok
+    const lastSlides = this.slides.splice(this.slides.length - this._SLIDES_NUMBER, this._SLIDES_NUMBER);
     this.slides = [].concat(lastSlides, this.slides);
 
-    lastSlides.forEach((slide) => this.list.insertAdjacentElement(`afterbegin`, slide));
-    console.log(this.list.querySelectorAll(`.slider__item`).length);
+    lastSlides.reverse().forEach((slide) => this.list.insertAdjacentElement(`afterbegin`, slide));
   }
 }
 
